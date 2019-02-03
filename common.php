30a7ab3a0bbd01f223f4ee3ff2cde6134e9ce13f
Dunja <43394060+dunjatomic23@users.noreply.github.com>
2019-02-03 09:11:35 -0500

    Update common.php

--- common.php
+++ common.php
@@ -35,17 +35,3 @@
 	header('Content-Type: text/html; charset=utf-8');
 	
 	session_start();
-	
-/*	
-if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
-    if(!headers_sent()) {
-        header("Status: 301 Moved Permanently");
-        header(sprintf(
-            'Location: https://%s%s',
-            $_SERVER['HTTP_HOST'],
-            $_SERVER['REQUEST_URI']
-        ));
-        exit();
-	}
-}
-*/
